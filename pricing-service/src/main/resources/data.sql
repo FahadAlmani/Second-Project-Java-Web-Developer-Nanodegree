4488bf9a0b510f9f81a66863fde21c210434d246
Fahad almani <Fahadalmana780@gmail.com>
2023-11-14 19:50:00 +0300

    Finish Second Project

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (1, 'USD',10);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (2, 'USD',20);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (3, 'USD',30);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (4, 'USD',40);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (5, 'USD',50);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (6, 'SAR',60);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (7, 'SAR',70);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (8, 'SAR',80);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (9, 'SAR',90);
+INSERT INTO PRICE (vehicle_Id, currency, price) VALUES (10, 'SAR',100);
+
